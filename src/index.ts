b2fd5df03f2cee21f8b0f03c5f7a8e2ceddf7d40
7anF <7anf2005hanafiah@gmail.com>
2024-07-25 11:21:27 -0700

    Product features

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
+import { seedInitialProducts } from './services/productService';
+import productRoute from './routes/productRoute';
 
 const app = express();
 const port = 3500;
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/ecommerce")
-            .then(() => console.log("Connected"))
-            .catch((err) => console.log(err));
+mongoose
+    .connect("mongodb://localhost:27017/ecommerce")
+    .then(() => console.log("Connected"))
+    .catch((err) => console.log(err));
 
-app.use('/user', userRoute)
+// Seed the products to database
+seedInitialProducts();
+
+app.use('/user', userRoute);
+app.use('/product', productRoute);
 
 app.listen(port, () => {
     console.log(`Server is running at:localhost:${port}`)
